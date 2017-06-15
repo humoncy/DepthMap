dea4678820644e60fe256556e723f8a225d1649d
humoncy <pengod4150@gmail.com>
2017-06-15 15:03:16 +0800

    test

--- DepthMap/Source.cpp
+++ DepthMap/Source.cpp
@@ -18,18 +18,18 @@ int main() {
 	// Show images
 	imshow("Input Image1", SrcImg1);
 	imshow("Input Image2", SrcImg2);
-	imshow("Input Image3", SrcImg3);
-	imshow("Input Image4", SrcImg4);
-	imshow("Input Image5", SrcImg5);
-	imshow("Input Image6", SrcImg6);
+	//imshow("Input Image3", SrcImg3);
+	//imshow("Input Image4", SrcImg4);
+	//imshow("Input Image5", SrcImg5);
+	//imshow("Input Image6", SrcImg6);
 
 	// Write output images
-	imwrite("Resources/tsukuba/scene1.row3.col1.jpg", SrcImg1);
+	/*imwrite("Resources/tsukuba/scene1.row3.col1.jpg", SrcImg1);
 	imwrite("Resources/tsukuba/scene1.row3.col2.jpg", SrcImg2);
 	imwrite("Resources/tsukuba/scene1.row3.col3.jpg", SrcImg3);
 	imwrite("Resources/tsukuba/scene1.row3.col4.jpg", SrcImg4);
 	imwrite("Resources/tsukuba/scene1.row3.col5.jpg", SrcImg5);
-	imwrite("Resources/tsukuba/truedisp.row3.col3.jpg", SrcImg6);
+	imwrite("Resources/tsukuba/truedisp.row3.col3.jpg", SrcImg6);*/
 
 	waitKey(0);
 	return 0;
